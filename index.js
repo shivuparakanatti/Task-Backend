5410ec5884a56f0ed92e7490c8dd87e51d17ae2c
shivuparakanatti <shivup5005@gmail.com>
2023-04-03 23:54:24 +0530

    commit

--- index.js
+++ index.js
@@ -2,27 +2,15 @@ var express = require('express')
 const app = express()
 const port = 3040
 const mongoose = require('mongoose')
-
 const router = require('../Tasks/config/routes')
-
 const configDb = require('./config/database')
 
 //set up db
 configDb()
 
+//application levcel middleware function
 app.use(express.json())
-app.use(router)      //application levcel middleware function
-
-
-
-
-
-
-
-
-
-
-
+app.use(router)      
 
 
 app.listen(port,()=>{
